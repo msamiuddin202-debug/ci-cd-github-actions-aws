4ea5cf31b29e57a33f2404980473aee66b34c200
Samiuddin Mohammed <msamiuddin202@gmail.com>
2026-04-14 15:21:47 +0000

    Test CI/CD deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("CI/CD Pipeline Working Successfully!");
+  res.end("🚀 Auto Deployed via GitHub Actions!");
 });
 
 server.listen(3000, () => {
